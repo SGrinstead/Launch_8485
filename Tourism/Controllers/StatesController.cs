76c14586ed306ef080544fa124d793f787a8341e
SGrinstead <seth.grinstead3299@gmail.com>
2023-07-27 08:48:25 -0700

    edit/update and i also added filtering by timezone in this branch on accident. oops

--- Tourism/Controllers/StatesController.cs
+++ Tourism/Controllers/StatesController.cs
@@ -14,9 +14,15 @@ namespace Tourism.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string? timezone)
         {
-            var states = _context.States.ToList();
+            var states = _context.States.AsEnumerable();
+            if(timezone != null)
+            {
+                states = states.Where(state => state.TimeZone == timezone);
+                ViewData["SearchTimezone"] = timezone;
+            }
+            ViewData["AllTimezones"] = _context.States.Select(state => state.TimeZone).Distinct().ToList();
             return View(states);
         }
 
@@ -40,6 +46,24 @@ namespace Tourism.Controllers
         {
             var state = _context.States.Find(stateId);
             return View(state);
+        }
+
+        [Route("/states/{stateId:int}/edit")]
+        public IActionResult Edit(int stateId)
+        {
+            var state = _context.States.Find(stateId);
+            return View(state);
+        }
+
+        [HttpPost]
+        [Route("/states/{stateId:int}")]
+        public IActionResult Update(int stateId, State state)
+        {
+            state.Id = stateId;
+            _context.States.Update(state);
+            _context.SaveChanges();
+
+            return Redirect($"/states/{stateId}");
         }
     }
 }
